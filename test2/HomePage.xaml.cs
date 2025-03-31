87ba355ca104e89b5f242571b854d0d11fd30914
Nate Lowry <nathaniel.lowry@maine.edu>
2025-03-31 12:59:06 -0400

    Initial File Commit

---
+++ test2/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.IO;
+
+namespace test2
+{
+    public partial class HomePage : ContentPage
+    {
+        int count = 0;
+
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ test2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.IO;
+
+namespace test2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = ((Entry)sender).Text;
+        }
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            string[] lines = { entry1.Text, entry2.Text, entry3.Text };
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "BossTest.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+        }
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (File.Exists(docPath + "/BossTest.txt"))
+            {
+                File.Delete(docPath + "/BossTest.txt");
+            }
+        }
+        void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = ((Editor)sender).Text;
+        }
+        void OnEditorCompleted(object sender, EventArgs e)
+        {
+            string text = ((Editor)sender).Text;
+        }
+    }
+
+}
